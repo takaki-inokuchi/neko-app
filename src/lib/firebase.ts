141427f1cb1d2954bd7a642dc99e343e4a6ae4a9
Takaki Inokuchi <inokutitakaki@yahoo.co.jp>
2025-08-17 03:05:32 +0900

    firebas修正

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBz0PuYNEBtYwGYAuEJrvEZHcgwe8USdWc",
-  authDomain: "neko-app-f24eb.firebaseapp.com",
-  projectId: "neko-app-f24eb",
-  storageBucket: "neko-app-f24eb.firebasestorage.app",
-  messagingSenderId: "1043281499227",
-  appId: "1:1043281499227:web:7f35403be27c8f19cb9b7e",
-  measurementId: "G-NV9YJL0VKG",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp(); //「!」もしデータが(length)存在していたらfirebasの情報を初期化　getAppは既に存在するデータを返す→初期化されていない場合に呼ぶとエラーになる。
